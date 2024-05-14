565ddb218985761aec225953c3e4396dd2566c5d
Matteo0421 <matteo.carola99@gmail.com>
2024-05-15 00:35:15 +0200

    [add]print

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,46 @@
 
 @section('content')
 
-<h1>Home</h1>
+<div class="container">
+    <h1>I VIAGGI DISPONIBILI</h1>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">AGENZIA</th>
+        <th scope="col">STAZIONE DI PARTENZA</th>
+        <th scope="col">STAZIONE DI RITORNO</th>
+        <th scope="col">ORARIO DI PARTENZA</th>
+        <th scope="col">ORARIO DI ARRIVO</th>
+        <th scope="col">DURATA</th>
+        <th scope="col">CODICE TRENO</th>
+        <th scope="col">NUMERO CARROZZA</th>
+        <th scope="col">IN ORARIO</th>
+        <th scope="col">CANCELLATO</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($trains as $train )
+
+
+        <tr>
+            <th >{{$train->agency}}</th>
+            <th >{{$train->departure_station}}</th>
+            <th >{{$train->arrival_station}}</th>
+            <th >{{$train->nationality}}</th>
+            <th >{{$train->departure_time}}</th>
+            <th >{{$train->arrival_time}}</th>
+            <th >{{$train->duration}}</th>
+            <th >{{$train->train_code}}</th>
+            <th >{{$train->carriage_number}}</th>
+            <th >{{$train->in_time}}</th>
+            <th >{{$train->cancelled}}</th>
+        </tr>
+        @endforeach
+
+
+    </tbody>
+  </table>
+</div>
 
 @endsection
